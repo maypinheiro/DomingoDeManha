1ecdce13f4571db8ef4c8e0f810b6b7af5bc7be1
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-29 10:25:29 -0300

    versao 1.0

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace DomingoDeManha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Diguite a hora de saida: ");
+                var tempo = Console.ReadLine();
+
+                TimeSpan horarioDeSaida = TimeSpan.Parse(tempo);
+                TimeSpan percurso = TimeSpan.Parse("01:00");
+                TimeSpan horaDoEncontro = TimeSpan.Parse("08:00");
+                TimeSpan horarioDeChegada = (horarioDeSaida + percurso);
+                TimeSpan chegada = horarioDeChegada - horaDoEncontro;
+                Console.WriteLine(Resultado(chegada));
+            }
+        }
+
+        public static string Resultado(TimeSpan chegada)
+        {
+            if (chegada.Hours < 0) return ($"Atraso máximo: {chegada.Minutes} minutos");
+            else if (chegada.Hours > 0) return ($"Atraso máximo: {chegada.Hours}:{chegada.Minutes}");
+            else return ($" Atraso máximo: {chegada.Minutes}");
+        }
+    }
+}
